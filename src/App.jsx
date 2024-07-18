2c69044e1258abeb9fa937bac1cff325afba38b6
snippy4 <thomascparsons@gmail.com>
2024-07-18 15:37:08 +0100

    React project created

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Header from './components/Header'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Header />
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header(){
+    return(
+        <div>
+            <h1 className="text-2xl flex items-center justify-center flex-col pt-20 pb-6 underline font-bold">This is a header</h1>
+        </div>
+    )
+}
+
+export default Header
