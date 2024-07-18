4aa92e73e20c33c24487fd6762a332e38f99271f
snippy4 <thomascparsons@gmail.com>
2024-07-18 16:44:05 +0100

    Added Header

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Header from './components/Header'
+import Header_Projects from './components/Header_Projects'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="bg-stone-950">
-    <Header />
+    <Header_Projects />
     </div>
   )
 }
