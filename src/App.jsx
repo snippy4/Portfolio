5081119a24afb54dc0a114f3d1ad8bd1a4eee224
snippy4 <thomascparsons@gmail.com>
2024-07-18 17:18:19 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Header from './components/Header'
 
+//index app
+
 function App() {
   const [count, setCount] = useState(0)
 
