e84340db35e4b2b079d0b9ec0f062719139583de
snippy4 <thomascparsons@gmail.com>
2024-07-30 14:12:45 +0100

    Mobile support + New project

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -16,23 +16,23 @@ import javascript from "/assets/javascript_.png"
 function Display(){
     return(
         <div className="p-10 overflow-hidden 2xl:overflow-visible flex flex-auto justify-center">
-            <div className='grid grid-cols-1 md:grid-rows-7 gap-5 md:max-h-2xl p-5 md:max-w-7xl md:max-h-screen md:grid-cols-3 grid-rows-auto'>
+            <div className='grid grid-cols-1 grid-rows-6 lg:grid-rows-7 gap-5 lg:max-h-2xl p-5 lg:max-w-7xl lg:max-h-screen lg:grid-cols-3 grid-rows-auto'>
 
-                <div className="bg-zinc-800 p-3 rounded-2xl col-span-2 flex flex-col max-h-60 relative row-span-2 animate-smooth_left">
+                <div className="bg-zinc-800 p-3 rounded-2xl col-span-2 flex flex-col relative lg:row-span-2 animate-smooth_left">
                     <h className="text-white font-semibold text-4xl p-2">My Introduction</h>
                     <p className="text-slate-400 p-2 text-xl">
-                        Hi! I'm Thomas, a computer science student and developer. Welcome to my portfolio website where you can find a summary of what I do and what I have worked on. I first started to learn programming when I was 16 and have been hooked on computer sceince since! Some of my other interests include: Maths, trading card games, and video games.
+                        Hi! I'm Thomas, a computer science student and developer. Welcome to my portfolio website where you can find a summary of what I do and what I have worked on. I first started to learn programming when I was 16 and have been hooked on computer sceince since. Some of my other interests include: maths, trading card games, and video games.
                     </p>
                 </div>
-                <div className="row-span-4 animate-smooth_top">
+                <div className="lg:row-span-4 row-span-2 animate-smooth_top">
                     <img src={lake_img} className="rounded-2xl object-cover size-full"></img>
                 </div>
 
-                <div className="bg-violet-300 p-3 rounded-2xl col-span-1 flex flex-col relative row-span-2 animate-smooth_left2">
+                <div className="bg-violet-300 p-3 rounded-2xl col-span-2 lg:col-span-1 flex flex-col relative lg:row-span-2 animate-smooth_left2">
                     <h className="text-black font-semibold text-4xl p-2">Computer Science Student & Hobbyist Developer</h>
                 </div>
 
-                <div className="bg-zinc-800 p-3 rounded-2xl col-span-1 row-span-2 flex flex-col relative row-span-3 animate-smooth_center">
+                <div className="bg-zinc-800 p-3 rounded-2xl col-span-1 flex flex-col relative lg:row-span-3 animate-smooth_center">
                     <h className="text-white font-semibold text-4xl p-2 flex flex-col">Thomas Parsons</h>
                     <div className="grid grid-cols-3 grid-rows-auto max-h-full">
                         <a className="p-2" href="https://www.linkedin.com/in/thomas-parsons-6b6964225/">
@@ -48,19 +48,21 @@ function Display(){
                     
                 </div>
 
-                <div className="bg-zinc-800 p-3 rounded-2xl col-span-1 flex flex-col relative row-span-3 max-h-full animate-smooth_bottom">
-                    <h1 className="text-white font-semibold text-4xl p-2">Education</h1>
-                    <h className="text-white font-semibold text-xl p-2 flex flex-col">University</h>
-                    <p className="text-slate-400 text-l p-2 flex flex-col">I am currently studying a MEng in Computer Science at University of Liverpool. I am on my fourth year of study and set to graduate in 2025</p>
-                    <h className="text-white font-semibold text-xl p-2 flex flex-col">A levels</h>
-                    <p className="text-slate-400 text-l p-2 flex flex-col">Maths - A* <br/> Further Maths - A <br/> Computer Science - A <br/> Economics - B <br/> I studied my A levels at Calday Grange Grammar School from 2019-2021</p>
+                <div className="bg-zinc-800 p-3 rounded-2xl col-span-2 lg:col-span-1 flex flex-col relative lg:row-span-3 max-h-full animate-smooth_bottom">
+                    <div>
+                        <h1 className="text-white font-semibold text-4xl p-2 flex flex-col">Education</h1>
+                        <h className="text-white font-semibold text-xl p-2 flex flex-col">University</h>
+                        <p className="text-slate-400 text-l p-2 flex flex-col">I am currently studying a MEng in Computer Science at University of Liverpool. I am on my fourth year of study and set to graduate in 2025</p>
+                        <h className="text-white font-semibold text-xl p-2 flex flex-col">A levels</h>
+                        <p className="text-slate-400 text-l p-2 flex flex-col">Maths - A* <br/> Further Maths - A <br/> Computer Science - A <br/> Economics - B <br/> I studied my A levels at Calday Grange Grammar School from 2019-2021</p>
+                    </div>
                 </div>
 
-                <div className="bg-violet-300 p-3 rounded-2xl col-span-1 flex flex-col relative animate-smooth_right2">
+                <div className="bg-violet-300 p-3 rounded-2xl col-span-2 lg:col-span-1 flex flex-col relative animate-smooth_right2">
                     <h className="text-black font-semibold text-l p-2">"If you want to know why the tree is growing upside down, ask the computer scienctists who introduced the convention"</h>
                 </div>
 
-                <div className="bg-zinc-800 p-3 rounded-2xl col-span-2 row-span-2 flex flex-col relative animate-smooth_right">
+                <div className="bg-zinc-800 p-3 rounded-2xl col-span-2 lg:row-span-2 flex flex-col relative animate-smooth_right">
                     <h className="text-white font-semibold text-4xl p-2">Technologies</h>
                     <div className="grid grid-cols-6 grid-rows-auto max-h-full">
 
--- src/components/ProjectDisplay.jsx
+++ src/components/ProjectDisplay.jsx
@@ -3,14 +3,16 @@ import { Link } from "react-router-dom";
 import portfolio_site from "/assets/Portfolio_site.png"
 import github from "/assets/github_.png"
 import link from "/assets/link_.png"
+import Bard from "/assets/Bard.png"
 
 function ProjectDisplay(){
     return(
         <div className="snap-y flex flex-auto items-center justify-center p-5">
-            <div className='grid grid-cols-1 md:grid-rows-auto gap-10 md:max-h-2xl p-5 md:max-w-7xl md:max-h-screen md:grid-cols-2 grid-rows-auto'>
+            <div className='grid grid-cols-1 md:grid-rows-auto gap-10 md:max-h-2xl p-5 md:max-w-7xl md:max-h-screen lg:grid-cols-2 grid-rows-auto'>
                 <div className="animate-smooth_left">
                     <img className="rounded-xl border border-zinc-700 size-full object-cover" src={portfolio_site}></img>
                 </div>
+
                 <div className="bg-zinc-800 p-3 rounded-2xl flex flex-col relative animate-smooth_top border border-zinc-700">
                     <h className="text-white font-semibold text-4xl p-2">Portfolio site</h>
                     <p className="text-slate-400 p-2 text-xl">
@@ -32,6 +34,29 @@ function ProjectDisplay(){
                         
                     </div>
                 </div>
+                    
+                <div className="animate-smooth_left2">
+                    <img className="rounded-xl border border-zinc-700 size-full object-fill max-h-60" src={Bard}></img>
+                </div>
+
+                <div className="bg-zinc-800 p-3 rounded-2xl flex flex-col relative animate-smooth_top border border-zinc-700">
+                    <h className="text-white font-semibold text-4xl p-2">Bard bot</h>
+                    <p className="text-slate-400 p-2 text-xl">
+                        This is a discord bot that I created for a private server with my friends. This bot will use RiotAPI to track each of my friends rank on league of legends, as well as a few interesting stats about their games, and post these in the form of an ordered leaderboard.
+                    </p>
+                    <div className="grid grid-cols-6 grid-rows-auto max-h-full p-2">
+                        <h className="text-black border border-black bg-white rounded-xl p-1 font-semibold justify-center flex hover:scale-90 transition">Python</h>
+                        <h className="text-black border border-black bg-white rounded-xl p-1 font-semibold justify-center flex hover:scale-90 transition">RiotAPI</h>
+                        <h className="text-black border border-black bg-white rounded-xl p-1 font-semibold justify-center flex hover:scale-90 transition">DiscordAPI</h>
+                    </div>
+                    <div className="grid grid-cols-1 grid-rows-auto p-1">
+                        <a href="https://github.com/snippy4/Bard">
+                            <img className="rounded-full hover:brightness-150 hover:drop-shadow-xl hover:scale-90 transition scale-75 max-h-60" src={github}></img>
+                        </a>
+                        
+                    </div>
+                </div>
+
             </div>
         </div>
     )
